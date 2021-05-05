ae004b806f7d1e8d60ba61ac89068989af5ab619
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2021-05-05 22:16:46 +0530

    Calculator Input Both Done, Power Button Left

---
+++ Calculator/index.js
@@ -0,0 +1,31 @@
+function deleteLastOne() {
+    var str = document.calc.txt.value;
+    var required = "";
+    for (var i = 0; i < str.length - 1; i++)
+        required += str[i];
+    document.calc.txt.value = required;
+}
+
+function Evaluation() {
+    var ans = eval(document.calc.txt.value);
+    document.calc.txt.value = ans;
+}
+
+var button = document.getElementById('button');
+var state = false;
+
+if (!state) {
+    document.getElementById('inp').style.backgroundColor = 'green' ; 
+}
+
+document.addEventListener("keyup", key => {
+    if (/^\d$/.test(key.key)) {
+        document.calc.txt.value += key.key;
+    }
+    if ((/[+-/*]/).test(key.key)) {
+        document.calc.txt.value += key.key;
+    }
+
+    if (key.key === "Enter")
+        Evaluation();
+});
